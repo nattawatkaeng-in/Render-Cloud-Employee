5d5ff35ee8a44de7bfa5251ef98d5165c7c6af32
ysmf <150317829+nattawatkaeng-in@users.noreply.github.com>
2025-08-22 10:27:53 +0700

    first commit

---
+++ myapp/__init__.py
@@ -0,0 +1,18 @@
+import os
+
+from flask import Flask 
+
+from .extensions import db
+from .routes import main
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    # postgresql://cloud_employee_manager_user:yYYz0c9QSIONdApJQTymN1n47gyIq4yV@dpg-d2jnnkggjchc73cqqm9g-a.oregon-postgres.render.com/cloud_employee_manager
+
+    db.init_app(app)
+
+    app.register_blueprint(main)
+
+    return app
