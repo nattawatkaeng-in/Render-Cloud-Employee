491e3a1f05ec3a3a606dd8cb0b1ee7afbe00c355
ysmf <150317829+nattawatkaeng-in@users.noreply.github.com>
2025-09-04 22:08:18 +0700

    add main blueprint

--- myapp/routes.py
+++ myapp/routes.py
@@ -4,7 +4,7 @@ from flask import Blueprint, redirect, url_for, render_template
 from .extensions import db
 from .models import User
 
-main = Blueprint('main', __name__)
+main = Blueprint('main', __name__, template_folder='templates')
 
 @main.route('/')
 def index():
