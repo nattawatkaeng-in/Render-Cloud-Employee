021de02e5744c0021a3e45658262d56f314178b4
ysmf <150317829+nattawatkaeng-in@users.noreply.github.com>
2025-09-04 23:44:37 +0700

    show and add method

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, url_for, render_template
+from flask import Blueprint, redirect, url_for, render_template, jsonify, request
 
 
 from .extensions import db
@@ -10,8 +10,19 @@ main = Blueprint('main', __name__, template_folder='templates')
 def index():
     return render_template("index.html")
 
-@main.route('/add/<username>')
-def add_user(username):
-    db.session.add(User(username=username))
+@main.route('/api/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    users_list = [{"id": user.id, "username": user.username} for user in users]
+    return jsonify(users_list)
+
+@main.route('/api/users/add', methods=['POST'])
+def add_user_api():
+    data = request.get_json()  # {"username": "newuser"}
+    if not data or "username" not in data:
+        return jsonify({"error": "Missing username"}), 400
+
+    new_user = User(username=data["username"])
+    db.session.add(new_user)
     db.session.commit()
-    return redirect(url_for("main.index"))
+    return jsonify({"message": "User added", "id": new_user.id})
