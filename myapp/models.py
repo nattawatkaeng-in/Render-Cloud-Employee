93493cd93c197ad4a6f02935a2c80e9da4702330
ysmf <150317829+nattawatkaeng-in@users.noreply.github.com>
2025-09-10 19:54:55 +0700

    changed to Employee table

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,22 @@
 from .extensions import db 
+from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50))
+    
+class Employee(db.Model):
+    __tablename__ = "employees"  # ชื่อตาราง (ไม่ใส่ก็ได้ จะ default ตาม class name)
+
+    employee_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    phone = db.Column(db.String(20), unique=True, nullable=True)
+    position = db.Column(db.String(50), nullable=True)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Employee {self.first_name} {self.last_name}>"
--- myapp/routes.py
+++ myapp/routes.py
@@ -2,19 +2,32 @@ from flask import Blueprint, redirect, url_for, render_template, jsonify, reques
 
 
 from .extensions import db
-from .models import User
+from .models import User, Employee
 
 main = Blueprint('main', __name__, template_folder='templates')
 
 @main.route('/')
 def index():
-    users = User.query.all()
-    return render_template("index.html", users=users)
+    employees = Employee.query.all()
+    return render_template("index.html", employees=employees)
 
 @main.route('/add', methods=["POST"])
-def add_user():
-    username = request.form.get("username")  # ดึงค่าจาก input form
-    if username:  # กันค่าที่ว่าง
-        db.session.add(User(username=username))
+def add_employee():
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    email = request.form.get("email")
+    phone = request.form.get("phone")
+    position = request.form.get("position")
+
+    if first_name and last_name and email:
+        new_employee = Employee(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            phone=phone,
+            position=position
+        )
+        db.session.add(new_employee)
         db.session.commit()
+
     return redirect(url_for("main.index"))
