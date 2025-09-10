b9c2fbd92ab344bcd0ebc30bc1452501eaad8ec9
ysmf <150317829+nattawatkaeng-in@users.noreply.github.com>
2025-09-10 17:10:06 +0700

    html page management

--- myapp/routes.py
+++ myapp/routes.py
@@ -6,14 +6,15 @@ from .models import User
 
 main = Blueprint('main', __name__, template_folder='templates')
 
-@main.route('/') 
+@main.route('/')
 def index():
-	users = User.query.all()
-	users_list_html = [f"<li>{ user.username }</li>" for user in users]
-	return f"<ul>{''.join(users_list_html)}</ul>"
+    users = User.query.all()
+    return render_template("index.html", users=users)
 
-@main.route('/add/<username>')
-def add_user(username):
-	db.session.add(User(username=username))
-	db.session.commit()
-	return redirect(url_for("main.index"))
+@main.route('/add', methods=["POST"])
+def add_user():
+    username = request.form.get("username")  # ดึงค่าจาก input form
+    if username:  # กันค่าที่ว่าง
+        db.session.add(User(username=username))
+        db.session.commit()
+    return redirect(url_for("main.index"))
